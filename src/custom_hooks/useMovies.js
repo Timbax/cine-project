9d46d4c474b69e7d4baa6f8192ccdd7df4aa7630
Daniel White <danielrx-8@hotmail.com>
2026-03-12 19:27:46 -0500

    Commit
    Commit

---
+++ src/custom_hooks/useMovies.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+const KEY = "f315037";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      /* callback?.(); */
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching data!");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie Not Found");
+          setMovies(data.Search || []);
+          /* console.log("Data Movie: ", data.Search); */
+        } catch (err) {
+          /*   console.error(err.message); */
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+}
